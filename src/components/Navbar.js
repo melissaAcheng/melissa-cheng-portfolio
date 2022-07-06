7d9896cd0f2459fa6ebe0021c4ceab052f44d67d
melissaAcheng <melcheng12@gmail.com>
2022-07-06 14:38:53 -0400

    responsive tablet and mobile layout for home page and hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useStaticQuery, graphql } from "gatsby";
+import Hamburger from "hamburger-react";
 
 const Navbar = ({ pageTitle, children }) => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
   const data = useStaticQuery(graphql`
     query {
       site {
@@ -13,31 +15,34 @@ const Navbar = ({ pageTitle, children }) => {
   `);
   return (
     <nav>
-      <h1>
-        <Link to="/">{data.site.siteMetadata.title}</Link>
-      </h1>
-      <div>
-        <ul>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/projects">Projects</Link>
-          </li>
-          <li>
-            <Link to="/music">Music</Link>
-          </li>
-          <li>
-            <Link to="/teaching">Teaching</Link>
-          </li>
-          <li>
-            <Link to="/blog">Blog</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
+      <div className="navHeader">
+        <h1>
+          <Link to="/">{data.site.siteMetadata.title}</Link>
+        </h1>
+        <div className="navMenu">
+          <Hamburger direction="left" toggled={navbarOpen} toggle={setNavbarOpen} />
+        </div>
       </div>
+      <ul className={`${navbarOpen ? "showMenu" : ""}`}>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link to="/music">Music</Link>
+        </li>
+        <li>
+          <Link to="/teaching">Teaching</Link>
+        </li>
+        <li>
+          <Link to="/blog">Blog</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
